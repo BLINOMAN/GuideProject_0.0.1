fb8f43ef2581e76562cc3d1bc193fd9f2f1684f3
Boris <sanyko724@gmail.com>
2022-05-01 20:36:05 +0500

    Added main files

---
+++ control.lua
@@ -0,0 +1,44 @@
+script.on_init(function()
+    global.players = {}
+end)
+script.on_event(defines.events.on_player_created, function(event)
+    local player = game.get_player(event.player_index)
+    global.players[player.index] = { controls_active = true }
+    
+    local screen_element = player.gui.screen
+    local main_frame = screen_element.add {
+        type = "frame",
+        name = "main_frame",
+        caption = { "titles.title" }
+    }
+    main_frame.style.size = { 385, 385 }
+    main_frame.auto_center = true
+
+    local content_frame = main_frame.add {
+        type = "frame",
+        name = "content_frame",
+        direction = "vertical",
+        style = "content_frame"
+    }
+
+    local controls_flow = content_frame.add {
+        type = "flow",
+        name = "controls_flow",
+        direction = "horizontal",
+        style = "controls_flow"
+    }
+    controls_flow.add {
+        type = "button",
+        name = "guide_button",
+        caption = { "conditions.deactivated" }
+    }
+end)
+
+script.on_event(defines.events.on_gui_click, function(event)
+    local player_global = global.players[event.player_index]
+    if event.element.name == "guide_button" then
+        player_global.controls_active = not player_global.controls_active
+        local control_toggle = event.element
+        control_toggle.caption = (player_global.controls_active) and { "conditions.deactivated" } or { "conditions.activated" }
+    end
+end)
---
+++ data.lua
@@ -0,0 +1,28 @@
+local styles = data.raw["gui-style"].default
+
+data:extend
+{
+    {
+        type = "custom-input",
+        name = "my-custom-input",
+        key_sequence = "I",
+        consuming = "none"
+    }
+}
+
+styles["content_frame"] = {
+    type = "frame_style",
+    parent = "inside_shallow_frame_with_padding",
+    vertically_stretchable = "on"
+}
+
+styles["controls_flow"] = {
+    type = "horizontal_flow_style",
+    vertical_align = "center",
+    horizontal_spacing = 16
+}
+
+styles["controls_textfield"] = {
+    type = "textbox_style",
+    width = 36
+}
