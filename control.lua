4eb3e72e2ae0e99e2915bbd3c11b4e221fa388c5
PavelK760 <104727697+PavelK760@users.noreply.github.com>
2022-05-02 19:39:45 +0500

    Update control.lua

--- control.lua
+++ control.lua
@@ -6,32 +6,14 @@ script.on_event(defines.events.on_player_created, function(event)
     global.players[player.index] = { controls_active = true }
     
     local screen_element = player.gui.screen
-    local main_frame = screen_element.add {
-        type = "frame",
-        name = "main_frame",
-        caption = { "titles.title" }
-    }
+    local main_frame = screen_element.add { type = "frame", name = "main_frame", caption = { "titles.title" } }
     main_frame.style.size = { 385, 385 }
     main_frame.auto_center = true
 
-    local content_frame = main_frame.add {
-        type = "frame",
-        name = "content_frame",
-        direction = "vertical",
-        style = "content_frame"
-    }
+    local content_frame = main_frame.add { type = "frame", name = "content_frame", direction = "vertical", style = "content_frame" }
 
-    local controls_flow = content_frame.add {
-        type = "flow",
-        name = "controls_flow",
-        direction = "horizontal",
-        style = "controls_flow"
-    }
-    controls_flow.add {
-        type = "button",
-        name = "guide_button",
-        caption = { "conditions.deactivated" }
-    }
+    local controls_flow = content_frame.add { type = "flow", name = "controls_flow", direction = "horizontal", style = "controls_flow"}
+    controls_flow.add { type = "button", name = "guide_button", caption = { "conditions.deactivated" } }
 end)
 
 script.on_event(defines.events.on_gui_click, function(event)
